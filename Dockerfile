c77d45b8fa792e6ed891b2c23a22d6925c936c97
Alexander Gård <alex.gaard@hotmail.com>
2022-05-30 06:32:22 +0200

    Bootstrap spring-boot project

---
+++ Dockerfile
@@ -0,0 +1,2 @@
+FROM ghcr.io/navikt/poao-baseimages/java:17
+COPY /target/amt-altinn-acl.jar app.jar
---
+++ src/main/kotlin/no/nav/amt_altinn_acl/config/ApplicationConfig.kt
@@ -0,0 +1,22 @@
+package no.nav.amt_altinn_acl.config
+
+import no.nav.common.log.LogFilter
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableJwtTokenValidation
+class ApplicationConfig {
+
+	@Bean
+	fun logFilterRegistrationBean(): FilterRegistrationBean<LogFilter> {
+		val registration = FilterRegistrationBean<LogFilter>()
+		registration.filter = LogFilter("amt-altinn-acl", false)
+		registration.order = 1
+		registration.addUrlPatterns("/*")
+		return registration
+	}
+
+}
