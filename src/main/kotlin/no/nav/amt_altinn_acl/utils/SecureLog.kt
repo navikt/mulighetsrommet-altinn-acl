4492289b598095c811cf6e29bf29c5ad602f1141
Alexander Gård <alex.gaard@hotmail.com>
2022-06-29 16:27:08 +0200

    Fix secure logger navn

--- src/main/kotlin/no/nav/amt_altinn_acl/utils/SecureLog.kt
+++ src/main/kotlin/no/nav/amt_altinn_acl/utils/SecureLog.kt
@@ -4,5 +4,5 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 object SecureLog {
-	val secureLog: Logger = LoggerFactory.getLogger("secureLog")
+	val secureLog: Logger = LoggerFactory.getLogger("SecureLog")
 }
