6d6be264eab580ff39a6317e2b38f69dc4b8c61c
Tormod Fossum <110398791+tofoss@users.noreply.github.com>
2023-10-03 09:02:26 +0200

    Fjern deprecated endepunkt og ubrukte tilganger (#98)

--- src/main/kotlin/no/nav/amt_altinn_acl/controller/RolleController.kt
+++ src/main/kotlin/no/nav/amt_altinn_acl/controller/RolleController.kt
@@ -5,11 +5,9 @@ import no.nav.amt_altinn_acl.service.AuthService
 import no.nav.amt_altinn_acl.service.RolleService
 import no.nav.amt_altinn_acl.utils.Issuer
 import no.nav.security.token.support.core.api.ProtectedWithClaims
-import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -18,18 +16,6 @@ class RolleController(
 	private val authService: AuthService,
 	private val rolleService: RolleService
 ) {
-
-	@Deprecated("Endepunktet vil snart bli slettet. Bytt til POST request",)
-	@GetMapping("/tiltaksarrangor")
-	@ProtectedWithClaims(issuer = Issuer.AZURE_AD)
-	fun hentTiltaksarrangorRollerDeprecated(@RequestParam norskIdent: String): HentRollerResponse {
-		authService.verifyRequestIsMachineToMachine()
-
-		val roller = rolleService.getRollerForPerson(norskIdent)
-		return HentRollerResponse(
-			roller.map { rolle -> HentRollerResponse.TiltaksarrangorRoller(rolle.organisasjonsnummer, rolle.roller.map { it.rolleType }) }
-		)
-	}
 
 	@PostMapping("/tiltaksarrangor")
 	@ProtectedWithClaims(issuer = Issuer.AZURE_AD)
