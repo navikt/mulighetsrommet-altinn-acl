adf2b281fdb84215c5484d87a741347493701bfa
tiafiring <tiafiring@gmail.com>
2023-11-24 07:27:54 +0100

    oppdaterer dependencies (#124)

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,12 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-    id("org.springframework.boot") version "3.1.5"
+    val kotlinVersion = "1.9.21"
+
+    id("org.springframework.boot") version "3.2.0"
     id("io.spring.dependency-management") version "1.1.4"
-    kotlin("jvm") version "1.9.20"
-    kotlin("plugin.spring") version "1.9.20"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
 }
 
 group = "no.nav.amt-altinn-acl"
